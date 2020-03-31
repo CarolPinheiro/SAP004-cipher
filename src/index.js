12d251d06d7525b7a74277f71144116bbe8776a1
Caroline Pinheiro <ommasshi@gmail.com>
2020-03-31 16:48:43 -0300

    Primeira versão do projeto

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+let encodeButton = document.getElementById("encode");
+let decodeButton = document.getElementById("decode");
+
+
+encodeButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    let dataInput = document.getElementById("data-input").value;
+    let offsetInput = parseInt(document.getElementById("offset").value);
+    document.getElementById("results").innerHTML = cipher.encode(offsetInput,dataInput);
+})
+
+decodeButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    let dataInput = document.getElementById("data-input").value;
+    let offsetInput = document.getElementById("offset").value;
+    document.getElementById("results").innerText = cipher.decode(offsetInput,dataInput)
+})
