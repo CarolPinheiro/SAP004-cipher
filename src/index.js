4473cdc045ff18220ee119c343cbb0aaad64a8d1
Caroline Pinheiro <ommasshi@gmail.com>
2020-05-24 14:13:35 -0300

    Corrigindo o Dom para reduzir repetições

--- src/index.js
+++ src/index.js
@@ -4,27 +4,24 @@ let encodeButton = document.getElementById("encode");
 let decodeButton = document.getElementById("decode");
 let cleanButton = document.getElementById("clean-button")
 let dataInput = document.getElementById("data-input");
-const offsetInput = parseInt(document.getElementById("offset").value);
+const offsetInput = document.getElementById("offset");
 
 
 encodeButton.addEventListener("click", function(event) {
     event.preventDefault();
-    
-    console.log(offsetInput.value)
-    //document.getElementById("results").innerHTML = encodeAndDecode(offsetInput,dataInput);
+    document.getElementById("results").innerHTML = encodeAndDecode(Number(offsetInput.value),dataInput.value);
 })
 
-// decodeButton.addEventListener("click", function(event) {
-//     let dataInput = document.getElementById("data-input");
-// let offsetInput = parseInt(document.getElementById("offset"));
-//     event.preventDefault();
-//     document.getElementById("results").innerText = encodeAndDecode(-offsetInput,dataInput)
-// })
+decodeButton.addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById("results").innerText = encodeAndDecode(-Number(offsetInput.value),dataInput.value)
+})
 
 cleanButton.addEventListener("click", function() {
     document.getElementById("data-input").innerHTML="";
     document.getElementById("offset").innerHTML="";
     document.getElementById("results").innerHTML= "";
 })
+
 
 
