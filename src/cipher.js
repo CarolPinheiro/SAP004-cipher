cc4a991bbcc758182e879c5ee9fbe4f49bdda201
Caroline Pinheiro <ommasshi@gmail.com>
2020-05-24 13:57:51 -0300

    Alterando a função de cipher e decipher para uma única

--- src/cipher.js
+++ src/cipher.js
@@ -1,51 +1,20 @@
-const cipher = {
-  encode: function (offset, string) {
-    let resultCipher = "";
-    if (typeof string != "string") {
-      throw new TypeError ();
-     }
+export const encodeAndDecode = (offset, string) => {
+  let resultDecipher = "";
+  const zAsciiUpperCase = 90;
+  const zAsciiLowerCase = 122;
+  if (typeof string != "string") {
+    throw new TypeError();
+  }
 
-    for (var i=0; i<string.length;i++) {
-        let encodeString = string.charCodeAt(i);
-        let num;
-         if (encodeString>=65 && (encodeString<=90)) {
-              num = encodeString-65;
-             resultCipher += String.fromCharCode(((num+offset)%26) + 65);
-         }
-         else if  (encodeString>=97 && (encodeString <=122)) {
-             num = encodeString-97;
-            resultCipher += String.fromCharCode(((num+offset)%26) + 97);
-         }
-         else {
-          resultCipher += String.fromCharCode(encodeString);
-         }
-    }
-    return resultCipher;
-  },
+  for (let item of string) {
+    let letterCharCode = item.charCodeAt();
+    resultDecipher += /[A-Z]/.test(item) ? cipher(letterCharCode, zAsciiUpperCase, offset) : /[a-z]/.test(item) ? cipher(letterCharCode, zAsciiLowerCase, offset) : item
 
-  decode: function (offset,string) {
-    let resultDecipher = "";
-    if (typeof string != "string") {
-      throw new TypeError ();
-     }
-
-    for (var i=0; i<string.length;i++) {
-        let decodeString = string.charCodeAt(i);
-        let num;
-         if (decodeString>=65 && (decodeString<=90)) {
-             num = decodeString-65;
-             resultDecipher += String.fromCharCode((num+(26-offset%26))%26 + 65);
-         }
-         else if  (decodeString>=97 && (decodeString<=122)) {
-                 num = decodeString-97;
-            resultDecipher += String.fromCharCode((num+(26-offset%26))%26 + 97);
-         }
-         else {
-          resultDecipher += String.fromCharCode(decodeString);
-         }
-         
-    }
-    return resultDecipher;
+  }
+  return resultDecipher
 }
-};
-export default cipher;
+
+const  cipher = (letterCharCode, value, offset) => {
+  let num = letterCharCode - value;
+  return String.fromCharCode((num + offset) % 26 + value);
+}
--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
-import cipher from './cipher.js';
+import {encodeAndDecode} from './cipher.js';
 
 let encodeButton = document.getElementById("encode");
 let decodeButton = document.getElementById("decode");
 let cleanButton = document.getElementById("clean-button")
+let dataInput = document.getElementById("data-input");
+const offsetInput = parseInt(document.getElementById("offset").value);
 
 
 encodeButton.addEventListener("click", function(event) {
     event.preventDefault();
-    let dataInput = document.getElementById("data-input").value;
-    let offsetInput = parseInt(document.getElementById("offset").value);
-    document.getElementById("results").innerHTML = cipher.encode(offsetInput,dataInput);
+    
+    console.log(offsetInput.value)
+    //document.getElementById("results").innerHTML = encodeAndDecode(offsetInput,dataInput);
 })
 
-decodeButton.addEventListener("click", function(event) {
-    event.preventDefault();
-    let dataInput = document.getElementById("data-input").value;
-    let offsetInput = document.getElementById("offset").value;
-    document.getElementById("results").innerText = cipher.decode(offsetInput,dataInput)
-})
+// decodeButton.addEventListener("click", function(event) {
+//     let dataInput = document.getElementById("data-input");
+// let offsetInput = parseInt(document.getElementById("offset"));
+//     event.preventDefault();
+//     document.getElementById("results").innerText = encodeAndDecode(-offsetInput,dataInput)
+// })
 
 cleanButton.addEventListener("click", function() {
     document.getElementById("data-input").innerHTML="";
